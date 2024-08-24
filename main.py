030807b0da8569f902de38bd2c6611a4b86703ae
Max <estate.murza@gmail.com>
2024-08-24 07:37:14 -0400

    WHITE = (255, 255, 255) BLACK = (0, 0, 0) RED = (255, 0, 0) GREEN = (0, 255, 0)

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import random
 
 pygame.init()
 
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Battle city")
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
 
 
